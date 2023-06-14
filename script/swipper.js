665a654e6e63605167b7d07cd02c17d9516fbcf0
pilentboy <sirxmahdi79@gmail.com>
2023-06-14 21:50:19 +0430

    V1.3 add swipper items

--- script/swipper.js
+++ script/swipper.js
@@ -1,5 +1,58 @@
-var swiper = new Swiper(".mySwiper", {
+
+
+if(window.innerWidth >=1284){
+  var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
     },
+    slidesPerView:3,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true
+    }
 });
+}else{
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    slidesPerView:1,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: true
+    }
+});
+}
+
+window.addEventListener("resize",()=>{
+  if(window.innerWidth >= 1284){
+      var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        slidesPerView:3,
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: true
+        }
+    });
+  }else{
+    var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      slidesPerView:1,
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: true
+      }
+  });
+  }
+
+
+})
+
